9099d48ef978c760ebd67d34da7d9c8caed76b4a
jhonatan9170 <65328388+jhonatan9170@users.noreply.github.com>
2024-02-06 15:42:52 -0500

    Initial Commit

---
+++ EncoraPrueba/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EncoraPrueba
+//
+//  Created by Whiz on 6/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
