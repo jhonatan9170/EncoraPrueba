84a371416b6c9f2a8d98e850d3c26d70e820f1ec
Jhonatan_Chavez <jacobosramirezm@gmail.com>
2024-09-17 17:34:56 -0500

    update visual detail

--- EncoraPrueba/Modules/Detail/DetailViewController.swift
+++ EncoraPrueba/Modules/Detail/DetailViewController.swift
@@ -3,8 +3,12 @@ import UIKit
 class DetailViewController: UIViewController {
 
     @IBOutlet private weak var titleLabel: UILabel!
-    @IBOutlet private weak var bodyLabel: UILabel!
-    @IBOutlet private weak var IDLabel: UILabel!
+    @IBOutlet private weak var officialNameLabel: UILabel!
+    @IBOutlet private weak var capitalLabel: UILabel!
+    @IBOutlet private weak var regionLabel: UILabel!
+    @IBOutlet private weak var populationLabel: UILabel!
+    @IBOutlet private weak var currencyLabel: UILabel!
+    @IBOutlet private weak var flagImageView: UIImageView!
     
     var presenter: DetailPresenterProtocol?
 
@@ -27,9 +31,14 @@ extension DetailViewController: DetailViewProtocol{
     
     func showCountryDetail() {
         guard let country = presenter?.country else { return }
-            
         titleLabel.text = country.name.common
-        bodyLabel.text = country.capital?.first ?? "No Capital"
-        IDLabel.text = country.region
+        officialNameLabel.text = "Nombre Oficial: \(country.name.official)"
+        capitalLabel.text = "Capital: \(country.capital?.first ?? "N/A")"
+        regionLabel.text = "Región: \(country.region)"
+        populationLabel.text = "Población: \(country.population.formattedWithSeparator())"
+        currencyLabel.text = "Moneda: \(country.currencies?.values.first?.name ?? "N/A") (\(country.currencies?.values.first?.symbol ?? ""))"
+        if let flagURL = country.flags.png, let url = URL(string: flagURL) {
+            flagImageView.loadImage(from: url)
+        }
     }
 }
---
+++ EncoraPrueba/Utilities/Extensions/ExtensionUIImageView.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension UIImageView {
+    func loadImage(from url: URL) {
+        DispatchQueue.global().async {
+            if let data = try? Data(contentsOf: url) {
+                DispatchQueue.main.async {
+                    self.image = UIImage(data: data)
+                }
+            }
+        }
+    }
+}
